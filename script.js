a409e16bab0577e253930e895fc22e3ceabc740a
Deviloper007 <63181536+Deviloper007@users.noreply.github.com>
2021-07-09 17:42:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let qq = document.getElementById("i").value;
+
+// qq=addEventListener("click",)
+
+function widths() {
+    console.log("c");
+    if(document.getElementById("i").value.style.width=="13px"){
+        document.getElementById("i").value.style.width="20px";
+    }
+}
